4ee9eafeac212e2e3c2b8b95d618843538ebf515
samana316 <samana316@users.noreply.github.com>
2016-08-26 10:58:43 -0400

    Linq; DefaultIfEmpty

---
+++ Enterprise.Core/Linq/Extensions/Implementations/DefaultIfEmpty.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Enterprise.Core.Linq
+{
+    internal sealed class DefaultIfEmpty<TSource> : AsyncEnumerableBase<TSource>
+    {
+        private readonly IAsyncEnumerable<TSource> source;
+
+        private readonly TSource defaultValue;
+
+        public DefaultIfEmpty(
+            IAsyncEnumerable<TSource> source, 
+            TSource defaultValue)
+        {
+            this.source = source;
+            this.defaultValue = defaultValue;
+        }
+
+        public override AsyncIterator<TSource> Clone()
+        {
+            return new DefaultIfEmpty<TSource>(this.source, this.defaultValue);
+        }
+
+        protected override async Task EnumerateAsync(
+            IAsyncYield<TSource> yield, 
+            CancellationToken cancellationToken)
+        {
+            using (var enumerator = source.GetAsyncEnumerator())
+            {
+                if (!await enumerator.MoveNextAsync(cancellationToken))
+                {
+                    await yield.ReturnAsync(this.defaultValue, cancellationToken);
+                    yield.Break();
+                }
+
+                do
+                {
+                    await yield.ReturnAsync(enumerator.Current, cancellationToken);
+                } while (await enumerator.MoveNextAsync(cancellationToken));
+            }
+        }
+    }
+}
