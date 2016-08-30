b2fa05c8f5ba8a0c553dfcc494602d51d1731be4
samana316 <samana316@users.noreply.github.com>
2016-08-30 16:51:39 -0400

    Linq; Cast & OfType

---
+++ Enterprise.Core/Linq/Extensions/Cast.cs
@@ -0,0 +1,21 @@
+﻿using Enterprise.Core.Utilities;
+
+namespace Enterprise.Core.Linq
+{
+    partial class AsyncEnumerable
+    {
+        public static IAsyncEnumerable<TResult> Cast<TResult>(
+            this IAsyncEnumerable source)
+        {
+            Check.NotNull(source, nameof(source));
+
+            var existing = source as IAsyncEnumerable<TResult>;
+            if (existing != null)
+            {
+                return existing;
+            }
+
+            return new Cast<TResult>(source);
+        }
+    }
+}
