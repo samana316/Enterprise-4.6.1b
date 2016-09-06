187c6049be7b98aae5ad79a1d699ab975353ebd4
samana316 <samana316@users.noreply.github.com>
2016-09-06 13:01:44 -0400

    Linq; ContainsAsync

---
+++ Enterprise.Core/Linq/Extensions/ContainsAsync.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Enterprise.Core.Utilities;
+
+namespace Enterprise.Core.Linq
+{
+    partial class AsyncEnumerable
+    {
+        public static Task<bool> ContainsAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            TSource value)
+        {
+            return source.ContainsAsync(value, CancellationToken.None);
+        }
+
+        public static Task<bool> ContainsAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            TSource value,
+            CancellationToken cancellationToken)
+        {
+            return source.ContainsAsync(value, null, cancellationToken);
+        }
+
+        public static Task<bool> ContainsAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            TSource value,
+            IEqualityComparer<TSource> comparer)
+        {
+            return source.ContainsAsync(value, comparer, CancellationToken.None);
+        }
+
+        public static async Task<bool> ContainsAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            TSource value,
+            IEqualityComparer<TSource> comparer,
+            CancellationToken cancellationToken)
+        {
+            Check.NotNull(source, nameof(source));
+
+            comparer = comparer ?? EqualityComparer<TSource>.Default;
+
+            using (var enumerator = source.GetAsyncEnumerator())
+            {
+                while (await enumerator.MoveNextAsync(cancellationToken))
+                {
+                    if (comparer.Equals(value, enumerator.Current))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
