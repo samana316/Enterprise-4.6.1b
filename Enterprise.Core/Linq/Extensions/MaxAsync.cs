b6e1c72f51069378d696de746ac6f8bf554e6e84
samana316 <samana316@users.noreply.github.com>
2016-09-07 13:59:17 -0400

    Linq; MaxAsync

---
+++ Enterprise.Core/Linq/Extensions/MaxAsync.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Enterprise.Core.Utilities;
+
+namespace Enterprise.Core.Linq
+{
+    partial class AsyncEnumerable
+    {
+        public static Task<TSource> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<TSource> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            CancellationToken cancellationToken)
+        {
+            Check.NotNull(source, nameof(source));
+
+            if (default(TSource) == null)
+            {
+                return source.NullableGenericMax(cancellationToken);
+            }
+
+            return source.GenericMaxAsync(cancellationToken);
+        }
+
+        public static Task<TResult> MaxAsync<TSource, TResult>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, TResult> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<TResult> MaxAsync<TSource, TResult>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, TResult> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<int> MaxAsync(
+            this IAsyncEnumerable<int> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<int> MaxAsync(
+            this IAsyncEnumerable<int> source,
+            CancellationToken cancellationToken)
+        {
+            return source.PrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<int?> MaxAsync(
+            this IAsyncEnumerable<int?> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<int?> MaxAsync(
+            this IAsyncEnumerable<int?> source,
+            CancellationToken cancellationToken)
+        {
+            return source.NullablePrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<int> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, int> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<int> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, int> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<int?> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, int?> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<int?> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, int?> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<long> MaxAsync(
+           this IAsyncEnumerable<long> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<long> MaxAsync(
+            this IAsyncEnumerable<long> source,
+            CancellationToken cancellationToken)
+        {
+            return source.PrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<long?> MaxAsync(
+            this IAsyncEnumerable<long?> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<long?> MaxAsync(
+            this IAsyncEnumerable<long?> source,
+            CancellationToken cancellationToken)
+        {
+            return source.NullablePrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<long> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, long> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<long> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, long> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<long?> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, long?> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<long?> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, long?> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<float> MaxAsync(
+           this IAsyncEnumerable<float> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<float> MaxAsync(
+            this IAsyncEnumerable<float> source,
+            CancellationToken cancellationToken)
+        {
+            return source.PrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<float?> MaxAsync(
+            this IAsyncEnumerable<float?> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<float?> MaxAsync(
+            this IAsyncEnumerable<float?> source,
+            CancellationToken cancellationToken)
+        {
+            return source.NullablePrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<float> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, float> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<float> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, float> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<float?> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, float?> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<float?> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, float?> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<double> MaxAsync(
+           this IAsyncEnumerable<double> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<double> MaxAsync(
+            this IAsyncEnumerable<double> source,
+            CancellationToken cancellationToken)
+        {
+            return source.PrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<double?> MaxAsync(
+            this IAsyncEnumerable<double?> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<double?> MaxAsync(
+            this IAsyncEnumerable<double?> source,
+            CancellationToken cancellationToken)
+        {
+            return source.NullablePrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<double> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, double> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<double> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, double> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<double?> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, double?> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<double?> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, double?> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<decimal> MaxAsync(
+           this IAsyncEnumerable<decimal> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<decimal> MaxAsync(
+            this IAsyncEnumerable<decimal> source,
+            CancellationToken cancellationToken)
+        {
+            return source.PrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<decimal?> MaxAsync(
+            this IAsyncEnumerable<decimal?> source)
+        {
+            return source.MaxAsync(CancellationToken.None);
+        }
+
+        public static Task<decimal?> MaxAsync(
+            this IAsyncEnumerable<decimal?> source,
+            CancellationToken cancellationToken)
+        {
+            return source.NullablePrimitiveMaxAsync(cancellationToken);
+        }
+
+        public static Task<decimal> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, decimal> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<decimal> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, decimal> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+
+        public static Task<decimal?> MaxAsync<TSource>(
+           this IAsyncEnumerable<TSource> source,
+           Func<TSource, decimal?> selector)
+        {
+            return source.MaxAsync(selector, CancellationToken.None);
+        }
+
+        public static Task<decimal?> MaxAsync<TSource>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, decimal?> selector,
+            CancellationToken cancellationToken)
+        {
+            return source.Select(selector).MaxAsync(cancellationToken);
+        }
+    }
+}
---
+++ Enterprise.Tests/Linq/AggregateAsync/MinAndMaxAsyncTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading.Tasks;
+using Enterprise.Core.Linq;
+using Enterprise.Tests.Linq.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Enterprise.Core.Linq.AsyncEnumerable;
+
+namespace Enterprise.Tests.Linq.MinMaxAsync
+{
+    [TestClass]
+    public sealed class AnyAndMaxAsyncTest
+    {
+        private const string CategoryLinqMinAsync = "Linq.MinAsync";
+
+        private const string CategoryLinqMaxAsync = "Linq.MaxAsync";
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task ArgumentValidation()
+        {
+            var source = default(IAsyncEnumerable<decimal>);
+            await source.MaxAsync();
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public async Task EmptySequence()
+        {
+            var empty = Empty<int>();
+            var nullableEmpty = Empty<int?>();
+
+            Assert.IsNull(await nullableEmpty.MaxAsync());
+            await empty.MaxAsync();
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        public async Task SequenceOfNull()
+        {
+            var source = new RealAsyncEnumerable<long?>(null, null, null);
+            Assert.IsNull(await source.MaxAsync());
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        public async Task Primitive()
+        {
+            var source = new RealAsyncEnumerable<float>(1, 2, 3, 0, 0, 3, 2, 1);
+            Assert.AreEqual(3, await source.MaxAsync());
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        public async Task PrimitiveNullable()
+        {
+            var source = new RealAsyncEnumerable<double?>(1, 2, 3, null, 0, null, 0, 3, 2, 1);
+            Assert.AreEqual(3, await source.MaxAsync());
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        public async Task PrimitiveProjection()
+        {
+            var source = new RealAsyncEnumerable<string>("", "Address", "Contract", "Binding");
+            Assert.AreEqual(8, await source.MaxAsync(x => x.Length));
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        public async Task PrimitiveProjectionNullable()
+        {
+            var source = new RealAsyncEnumerable<string>("", "Address", "Contract", "Binding");
+            Assert.AreEqual(1, await source.MaxAsync(x => x.Length <= 0 ? default(int?) : 1));
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        public async Task Generic()
+        {
+            var date1 = DateTime.Today;
+            var date2 = DateTime.Now;
+            var source = new RealAsyncEnumerable<DateTime>(date2);
+            Assert.AreEqual(date2, await source.MaxAsync());
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        public async Task GenericNullable()
+        {
+            var source = new RealAsyncEnumerable<string>("", "Address", "Contract", "Binding");
+            Assert.AreEqual("Contract", await source.MaxAsync());
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryLinqMaxAsync)]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task IncomparableValues()
+        {
+            var source = Create<Func<string, bool>>(async (yield, cancellationToken) => 
+            {
+                await yield.ReturnAsync(string.IsNullOrEmpty, cancellationToken);
+                await yield.ReturnAsync(string.IsNullOrWhiteSpace, cancellationToken);
+            });
+
+            var max = await source.MaxAsync();
+        }
+    }
+}
