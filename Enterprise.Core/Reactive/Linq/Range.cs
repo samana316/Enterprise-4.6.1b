e0329d41806220e765a07bf4316a168c42390c10
samana316 <samana316@users.noreply.github.com>
2016-09-12 14:12:19 -0400

    Reactive; Range

---
+++ Enterprise.Core/Reactive/Linq/Range.cs
@@ -0,0 +1,32 @@
+﻿using Enterprise.Core.Reactive.Linq.Implementations;
+using Enterprise.Core.Resources;
+
+namespace Enterprise.Core.Reactive.Linq
+{
+    partial class AsyncObservable
+    {
+        public static IAsyncObservable<int> Range(
+            int start,
+            int count)
+        {
+            var num = (long)start + count - 1L;
+
+            if (count < 0 || num > int.MaxValue)
+            {
+                throw Error.ArgumentOutOfRange(nameof(count));
+            }
+
+            if (count == 0)
+            {
+                return Empty<int>();
+            }
+
+            if (count == 1)
+            {
+                return Return(start);
+            }
+
+            return new Range(start, count);
+        }
+    }
+}
---
+++ Enterprise.Tests/Reactive/Range/RangeTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Enterprise.Core.Reactive.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Enterprise.Tests.Reactive.Range
+{
+    [TestClass]
+    public class RangeTest
+    {
+        private const int DefaultTimeout = 1000;
+        private const string CategoryReactiveRange = "Reactive.Range";
+
+        [TestMethod]
+        [TestCategory(CategoryReactiveRange)]
+        public async Task SimpleValidRange()
+        {
+            var source = AsyncObservable.Range(1, 3);
+            Assert.IsTrue(await source.SequenceEqual(new[] { 1, 2, 3 }));
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryReactiveRange)]
+        public async Task AllowNegativeStart()
+        {
+            var source = AsyncObservable.Range(-1, 3);
+            Assert.IsTrue(await source.SequenceEqual(new[] { -1, 0, 1 }));
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryReactiveRange)]
+        public async Task ZeroCountShouldBeEmpty()
+        {
+            var source = AsyncObservable.Range(int.MinValue, 0);
+            Assert.IsTrue(await source.SequenceEqual(new int[0]));
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryReactiveRange)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CountCantBeNegative()
+        {
+            var source = AsyncObservable.Range(int.MinValue, -1);
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryReactiveRange)]
+        public async Task CanIncludeMaxValue()
+        {
+            var source = AsyncObservable.Range(int.MaxValue, 1);
+            Assert.IsTrue(await source.SequenceEqual(new[] { int.MaxValue }));
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryReactiveRange)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CantExceedMaxValue()
+        {
+            var source = AsyncObservable.Range(int.MaxValue, 2);
+        }
+
+        [TestMethod]
+        [TestCategory(CategoryReactiveRange)]
+        [Timeout(DefaultTimeout)]
+        public void SmallStartWithLargeCount()
+        {
+            var count = 100000;
+            var source = AsyncObservable.Range(1, count);
+            var list = new List<int>(count);
+            list.AddRange(source.AsEnumerable());
+            Assert.AreEqual(count, list.Count);
+
+            var expected = Enumerable.Range(1, count).ToList();
+            CollectionAssert.AreEquivalent(expected, list);
+        }
+    }
+}
